5bfb7a6c91f3d4057f87ebc21d3bd7a1b0fcdd56
lateminer <9951982+lateminer@users.noreply.github.com>
2023-12-17 00:13:27 +0100

    Add support for multiple addresses

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,11 @@ struct Coin {
     name: String,
     ticker: String,
     api: ApiType,
+    addresses: Vec<Address>,
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
+struct Address {
     address: String,
     #[serde(default)]
     balance: Option<f32>,
@@ -36,13 +41,18 @@ lazy_static! {
 fn load_coins() -> Vec<Coin> {
     let toml_str = match fs::read_to_string("coins.toml") {
         Ok(content) => content,
-        Err(_) => panic!("Error reading the config file"),
+        Err(e) => panic!("Error reading the config file: {:?}", e),
     };
 
     let coins: Result<Coins, toml::de::Error> = toml::from_str(&toml_str);
 
     match coins {
-        Ok(coins) => coins.coins,
+        Ok(mut coins) => {
+            for coin in coins.coins.iter_mut() {
+                coin.addresses = coin.addresses.iter().map(|addr| Address { address: addr.address.clone(), balance: None, last_block_timestamp: None }).collect();
+            }
+            coins.coins
+        }
         Err(e) => panic!("Error parsing TOML: {:?}", e),
     }
 }
@@ -50,41 +60,43 @@ fn load_coins() -> Vec<Coin> {
 async fn update_coins_list() -> Result<(), Box<dyn std::error::Error>> {
     let mut coins_list = COINS.lock().unwrap();
     for coin in &mut *coins_list {
-        match coin.api {
-            ApiType::Chainz => {
-                let url = format!(
-                    "https://chainz.cryptoid.info/{}/api.dws?q=addressinfo&a={}",
-                    &coin.ticker.to_lowercase(),
-                    &coin.address
-                );
-                let resp = reqwest::get(url).await?;
-                let res = resp.text().await?;
+        for addr in &mut coin.addresses {
+            match coin.api {
+                ApiType::Chainz => {
+                    let url = format!(
+                        "https://chainz.cryptoid.info/{}/api.dws?q=addressinfo&a={}",
+                        &coin.ticker.to_lowercase(),
+                        &addr.address
+                    );
+                    let resp = reqwest::get(url).await?;
+                    let res = resp.text().await?;
 
-                let json_data: serde_json::Value = serde_json::from_str(&res)?;
-                if let Some(balance) = json_data.get("balance").and_then(|b| b.as_f64()) {
-                    coin.balance = Some(balance as f32);
+                    let json_data: serde_json::Value = serde_json::from_str(&res)?;
+                    if let Some(balance) = json_data.get("balance").and_then(|b| b.as_f64()) {
+                        addr.balance = Some(balance as f32);
+                    }
+
+                    if let Some(last_timestamp) = json_data
+                        .get("lastBlockTimestamp")
+                        .and_then(|ts| ts.as_i64())
+                    {
+                        addr.last_block_timestamp = Some(last_timestamp as u64);
+                    }
                 }
+                ApiType::Blnscan => {
+                    let url = "https://blnexplorer.io/api/account/".to_owned() + &addr.address;
+                    let resp = reqwest::get(url).await?;
+                    let res = resp.text().await?;
 
-                if let Some(last_timestamp) = json_data
-                    .get("lastBlockTimestamp")
-                    .and_then(|ts| ts.as_i64())
-                {
-                    coin.last_block_timestamp = Some(last_timestamp as u64);
-                }
-            }
-            ApiType::Blnscan => {
-                let url = "https://blnexplorer.io/api/account/".to_owned() + &coin.address;
-                let resp = reqwest::get(url).await?;
-                let res = resp.text().await?;
-
-                let json_data: serde_json::Value = serde_json::from_str(&res)?;
-                if let Some(txn) = json_data.get("txns").and_then(|txns| txns.get(0)) {
-                    if let Some(last_timestamp) = txn.get("time") {
-                        if let Some(timestamp) = last_timestamp
-                            .as_i64()
-                            .or_else(|| last_timestamp.as_str().and_then(|s| s.parse().ok()))
-                        {
-                            coin.last_block_timestamp = Some(timestamp as u64);
+                    let json_data: serde_json::Value = serde_json::from_str(&res)?;
+                    if let Some(txn) = json_data.get("txns").and_then(|txns| txns.get(0)) {
+                        if let Some(last_timestamp) = txn.get("time") {
+                            if let Some(timestamp) = last_timestamp
+                                .as_i64()
+                                .or_else(|| last_timestamp.as_str().and_then(|s| s.parse().ok()))
+                            {
+                                addr.last_block_timestamp = Some(timestamp as u64);
+                            }
                         }
                     }
                 }
@@ -194,54 +206,85 @@ fn get_time_since_last_activity(last_timestamp: u64) -> String {
 
 fn format_timestamp(timestamp: u64) -> String {
     if let Some(naivedatetime) = NaiveDateTime::from_timestamp_opt(timestamp as i64, 0) {
-        let datetime = DateTime::<Utc>::from_naive_utc_and_offset(
-            naivedatetime,
-            Utc,
-        );
+        let datetime = DateTime::<Utc>::from_naive_utc_and_offset(naivedatetime, Utc);
         datetime.format("%Y-%m-%d %H:%M:%S").to_string()
     } else {
         String::from("?")
     }
 }
 
-fn format_coins(coins: &[Coin]) -> String {
-    coins.iter().map(|coin| {
-        format!(
-            r#"
+fn format_addresses(coin: &Coin, addresses: &[Address]) -> String {
+    addresses
+        .iter()
+        .map(|address| {
+            format!(
+                r#"
                 <div class="row">
-                    <h2><img src="{}" alt="{}">{}</h2>
                     Address: <a href="{}">{}</a><br>
                     Balance: {}<br>
                     Last Active On: {}<br>
                     Time Since Last Activity: {}
                 </div>
             "#,
-            if coin.api == ApiType::Chainz {
-                format!("https://chainz.cryptoid.info/logo/{}.png", coin.ticker.to_lowercase())
-            } else if coin.api == ApiType::Blnscan {
-                "https://blnexplorer.io/favicon.ico".to_string()
-            }
-            else {
-                // Return an empty string for now
-                String::new()
-            },
-            coin.name,
-            coin.name,
-            if coin.api == ApiType::Chainz {
-                format!("https://chainz.cryptoid.info/{}/address.dws?{}.htm", coin.ticker.to_lowercase(), coin.address)
-            } else if coin.api == ApiType::Blnscan {
-                format!("https://blnexplorer.io/{}", coin.address)
-            }
-            else {
-                // Return an empty string for now
-                String::new()
-            },
-            coin.address,
-            coin.balance.map_or("?".to_string(), |balance| balance.to_string() + " " + &coin.ticker),
-            coin.last_block_timestamp.map_or("?".to_string(), |timestamp| format_timestamp(timestamp)),
-            if let Some(last_timestamp) = coin.last_block_timestamp { get_time_since_last_activity(last_timestamp) } else { "?".to_string() },
-        )
-    }).collect()
+                if coin.api == ApiType::Chainz {
+                    format!(
+                        "https://chainz.cryptoid.info/{}/address.dws?{}.htm",
+                        coin.ticker.to_lowercase(),
+                        address.address
+                    )
+                } else if coin.api == ApiType::Blnscan {
+                    format!("https://blnexplorer.io/{}", address.address)
+                } else {
+                    // Return an empty string for now
+                    String::new()
+                },
+                address.address,
+                address
+                    .balance
+                    .map_or("?".to_string(), |balance| balance.to_string()
+                        + " "
+                        + &coin.ticker),
+                address
+                    .last_block_timestamp
+                    .map_or("?".to_string(), |timestamp| format_timestamp(timestamp)),
+                if let Some(last_timestamp) = address.last_block_timestamp {
+                    get_time_since_last_activity(last_timestamp)
+                } else {
+                    "?".to_string()
+                },
+            )
+        })
+        .collect()
+}
+
+fn format_coins(coins: &[Coin]) -> String {
+    coins
+        .iter()
+        .map(|coin| {
+            format!(
+                r#"
+                <div class="row">
+                    <h2><img src="{}" alt="{}">{}</h2>
+                    {}
+                </div>
+            "#,
+                if coin.api == ApiType::Chainz {
+                    format!(
+                        "https://chainz.cryptoid.info/logo/{}.png",
+                        coin.ticker.to_lowercase()
+                    )
+                } else if coin.api == ApiType::Blnscan {
+                    "https://blnexplorer.io/favicon.ico".to_string()
+                } else {
+                    // Return an empty string for now
+                    String::new()
+                },
+                coin.name,
+                coin.name,
+                format_addresses(coin, &coin.addresses),
+            )
+        })
+        .collect()
 }
 
 #[actix_web::main]
